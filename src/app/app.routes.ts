736a4a913bb795ff89467313cfb0a3ef7bbbd60b
Shreyash Anand <anandshreyash747@gmail.com>
2026-02-03 15:54:32 +0530

    Implement routing with AuthGuard and update component templates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,36 @@ import { BedBoardPage } from './bed-board/pages/bed-board-page/bed-board-page';
 import { PatientListPage } from './patients/pages/patient-list-page/patient-list-page';
 import { AuditLogPage } from './audit/pages/audit-log-page/audit-log-page';
 import { Login } from './auth/pages/login/login';
+import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-    {path: '',redirectTo: 'bed-board', pathMatch: 'full'},
-    {path: 'bed-board', component:BedBoardPage},
-    {path: 'patients', component:PatientListPage},
-    {path: 'audit', component:AuditLogPage},
-    {path: 'login', component:Login}
+    {
+        path: 'login',
+         component:Login
+    },
+    {
+        path: 'bed-board',
+        component:BedBoardPage,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'patients', 
+        component:PatientListPage,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'audit', 
+        component:AuditLogPage,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: '',
+        redirectTo: 'bed-board',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'bed-board'
+    }
+    
 ];
